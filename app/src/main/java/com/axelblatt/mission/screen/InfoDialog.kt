6a7093cfbb9bf629a31e821c4bb0e335cbbda7c7
Fedor Egorov <axelblatt@>
2025-08-13 10:11:18 +0300

    Initial commit

--- app/src/main/java/com/axelblatt/mission/screen/InfoDialog.kt
+++ app/src/main/java/com/axelblatt/mission/screen/InfoDialog.kt
@@ -43,7 +43,7 @@ fun InfoDialog(
             Row(modifier.fillMaxWidth(0.9f), horizontalArrangement = Arrangement.SpaceBetween) {
                 Column {
                     Text(text = "Mission")
-                    Text(text = "Everyday habit tracker,\nsimple as possible.", fontSize = 16.sp, lineHeight = 18.sp)
+                    Text(text = "Daily habit tracker,\nsimple as possible.", fontSize = 16.sp, lineHeight = 18.sp)
                     Text(text = "version $ver", fontSize = 16.sp)
                 }
                 Image(
